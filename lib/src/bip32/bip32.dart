45b57eeb4b646eba33201319c18a6bee9edc1a6c
Ravi Teja Gudapati <tejainece@gmail.com>
2021-06-16 01:30:17 +0200

    Extended key encoding

--- lib/src/bip32/bip32.dart
+++ lib/src/bip32/bip32.dart
@@ -4,6 +4,7 @@ import 'dart:typed_data';
 import 'package:hdwallet/src/util/util.dart';
 import 'package:ninja/ninja.dart';
 import 'package:web3dart/crypto.dart';
+import 'package:secp256k1/src/base.dart' as base;
 
 class PrivateKey {
   final BigInt privateKey;
@@ -36,12 +37,26 @@ class PrivateKey {
   }
 }
 
+class ExtendedKeyProps {
+  final int depth;
+  final Uint8List parentFingerprint;
+  final BigInt index;
+
+  ExtendedKeyProps(
+      {required this.depth,
+      required this.parentFingerprint,
+      required this.index});
+}
+
 class ExtendedPrivateKey extends PrivateKey {
   final Uint8List chainCode;
 
-  ExtendedPrivateKey(BigInt privateKey, this.chainCode) : super(privateKey);
+  final ExtendedKeyProps? props;
 
-  ExtendedPrivateKey generateHardenedChildKey(int index) {
+  ExtendedPrivateKey(BigInt privateKey, this.chainCode, {this.props})
+      : super(privateKey);
+
+  ExtendedPrivateKey generateHardenedChildKey(BigInt index) {
     if (index < hardenBit) {
       throw ArgumentError(
           'index should be greater than or equal to $hardenBit');
@@ -49,21 +64,53 @@ class ExtendedPrivateKey extends PrivateKey {
     final data = Uint8List(37);
     data[0] = 0x00;
     data.setRange(1, 33, privateKey.toBytes());
-    data.buffer.asByteData().setUint32(33, index);
+    data.setRange(33, 37, index.toBytes(outLen: 4));
     final whole = Uint8List.fromList(hmacSHA512(this.chainCode, data));
-    final key = bytesToBigInt(whole.sublist(0, 32));
+    final key =
+        (bytesToBigInt(whole.sublist(0, 32)) + privateKey) % base.secp256k1.n;
     final chainCode = whole.sublist(32);
-    // TODO check that key is less than G
-    // TODO check that chainCode is less than G
     return ExtendedPrivateKey(key, chainCode);
   }
 
-  ExtendedPublicKey generateNonHardenedChildKey(int index) {
+  ExtendedPublicKey generateNonHardenedChildKey(BigInt index) {
     if (index >= hardenBit) {
       throw ArgumentError('index should be less than $hardenBit');
     }
+    final data = Uint8List(37);
+    data.setRange(0, 33, publicKey.encodeIntoBytes());
+    data.setRange(33, 37, index.toBytes(outLen: 4));
+    final whole = Uint8List.fromList(hmacSHA512(this.chainCode, data));
+    final key = bytesToBigInt(whole.sublist(0, 32));
+    final chainCode = whole.sublist(32);
     // TODO
 
+    throw UnimplementedError();
+  }
+
+  BigInt get checksum {
+    // TODO
+    throw UnimplementedError();
+  }
+
+  Uint8List encodeBytes({ExtendedKeyProps? props}) {
+    props ??= this.props;
+    if(props == null) {
+      throw Exception('props not found');
+    }
+    final bytes = Uint8List(82);
+    bytes.setRange(0, 4, [0x04, 0x88, 0xad, 0xe4]);
+    bytes[4] = props.depth;
+    bytes.setRange(5, 9, props.parentFingerprint);
+    bytes.setRange(9, 13, props.index.toBytes(outLen: 4));
+    bytes.setRange(13, 45, chainCode);
+    bytes.setRange(45, 78, privateKey.toBytes(outLen: 33));
+    bytes.setRange(78, 82, checksum.toBytes(outLen: 4));
+    return bytes;
+  }
+
+  String encode({ExtendedKeyProps? props}) {
+    final bytes = encodeBytes(props: props);
+    // TODO base58 encode
     throw UnimplementedError();
   }
 }
@@ -101,22 +148,25 @@ class PublicKey {
 
   PublicKey(this.x, this.y);
 
-  String encode({bool compressed = true}) {
+  Uint8List encodeIntoBytes({bool compressed = true}) {
     final pointLen = 32;
     final xBytes = bigIntToBytes(x, outLen: pointLen);
 
-    if(!compressed) {
+    if (!compressed) {
       final yBytes = bigIntToBytes(y, outLen: pointLen);
-      final bytes = Uint8List(2 * pointLen + 1);
+      final bytes = Uint8List(33);
       bytes[0] = 0x04;
       bytes.setAll(1, xBytes);
       bytes.setAll(pointLen + 1, yBytes);
-      return bytesToHex(bytes);
+      return bytes;
     } else {
       // TODO
       throw UnimplementedError();
     }
   }
+
+  String encode({bool compressed = true}) =>
+      bytesToHex(encodeIntoBytes(compressed: compressed));
 
   @override
   String toString() => 'PublicKey($x, $y)';
@@ -127,4 +177,4 @@ class ExtendedPublicKey extends PublicKey {
   // TODO
 }
 
-const hardenBit = 0x80000000;
+final hardenBit = BigInt.tryParse('0x80000000', radix: 16)!;
